7a65ee2addb00fa2edb79ac34252c503faa88848
pimentelpedrop <pedrop.pimentel@hotmail.com>
2021-08-05 18:35:48 -0300

    estrutura inicial spring initialzr

---
+++ src/main/java/br/unesp/soo/grupocinco/grupocinco/GrupocincoApiApplication.java
@@ -0,0 +1,13 @@
+package br.unesp.soo.grupocinco.grupocinco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupocincoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupocincoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unesp/soo/grupocinco/grupocinco/GrupocincoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unesp.soo.grupocinco.grupocinco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupocincoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
